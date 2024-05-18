11e0ca33190abc3e22cd835fb608da36cb4e3d06
Zynox The Dev <116634521+Panchalparth471@users.noreply.github.com>
2024-05-18 09:08:22 +0530

    Update Horoscope.js

--- server/Controllers/Horoscope.js
+++ server/Controllers/Horoscope.js
@@ -1,43 +1,32 @@
-const puppeteer = require('puppeteer');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 async function scrapeHoroscope(req, res) {
     try {
-    
-        const { sign,day } = req.body;
-       const browser = await puppeteer.launch({
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
-    executablePath: puppeteer.executablePath(),
-  });
-        const page = await browser.newPage();
-        await page.setDefaultNavigationTimeout(0);
-
+        const { sign, day } = req.body;
         const url = `https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-${day}.aspx?sign=${sign}`;
-        await page.goto(url);
+
+        // Fetching HTML content of the horoscope page
+        const response = await axios.get(url);
+        const html = response.data;
+
+        // Parsing HTML content using Cheerio
+        const $ = cheerio.load(html);
 
         // Selector for horoscope content might change on the website. Update as needed.
-        const horoscopeText = await page.evaluate(() => document.querySelector('.main-horoscope p').innerText);
+        const horoscopeText = $('.main-horoscope p').text();
 
-        await browser.close();
         return res.status(200).json({
             success: true,
             data: { sign, horoscopeText }
-       })
-    }
-    catch (e)
-    {
+        });
+    } catch (e) {
         console.log(e);
         return res.status(500).json({
             success: false,
-            message:"Something went wrong"
-        })
+            message: "Something went wrong"
+        });
     }
-    
-
 }
 
 module.exports = { scrapeHoroscope };
